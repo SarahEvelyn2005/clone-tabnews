5241b261056fa485f653b81681723e74d7cfc533
sarah evelyn <evelynpinheiro2005@gmail.com>
2024-06-06 11:14:43 -0300

    desafio da pessoa amada

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>oi meu amor eu te amo mto. vc é meu mundo, vou passar por todos e desafios e dificuldades que vamos passar não importa oq seja meu amor.bom trabalho estou aqui viu   </h1>
+}
+
+
+export default Home; 
