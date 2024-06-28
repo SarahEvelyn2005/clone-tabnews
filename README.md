a7254057c4b4457a8101fa0fd1337e8cbc93e402
sarah evelyn <evelynpinheiro2005@gmail.com>
2024-06-28 14:01:41 -0300

    adicionar scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 projeto fullstack (treinamento)
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1>quero ir ficar com vc  </h1>
+  return <h1>quero ir ficar com vc </h1>;
 }
 
-
-export default Home; 
+export default Home;
