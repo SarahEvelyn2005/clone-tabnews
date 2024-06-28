3a161cb5b2c63b1247e4207370ad426858364637
sarah evelyn <evelynpinheiro2005@gmail.com>
2024-06-28 13:41:37 -0300

    adicionar arquivo `.editoconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>oi meu amor eu te amo mto. vc é meu mundo, vou passar por todos e desafios e dificuldades que vamos passar não importa oq seja meu amor.bom trabalho estou aqui viu   </h1>
+    return <h1>quero ir ficar com vc  </h1>
 }
 
 
